4f71db0fc8632fd7f7abbc2e781d2a0f9ee2fa97
rogeruk1977 <rogeruk1977@gmail.com>
2018-06-10 21:14:59 +0100

    Initial commit

---
+++ includes/db.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Roger Work
+ * Date: 31/05/2018
+ * Time: 13:16
+ */
+
+$db['db_host'] = 'localhost';
+$db['db_user'] = 'root';
+$db['db_pass'] = '';
+$db['db_name'] = 'cms';
+
+foreach($db as $key=>$value){
+    define(strtoupper($key), $value);
+}
+
+$connection = mysqli_connect(DB_HOST,DB_USER, DB_PASS,DB_NAME);
+
+
+//if($connection) {
+//
+//    echo "We are connected";
+//}else
+
+
+    ?>
